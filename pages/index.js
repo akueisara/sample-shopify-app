2b25c4009f38ab6a6d1c428fff3bd415dbd6a3d9
Kuei-Jung Hu <akueisara39@gmail.com>
2020-11-10 22:25:34 +0900

    Save product IDs

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,19 @@
 import React, { useState } from 'react';
 import { EmptyState, Layout, Page } from "@shopify/polaris";
 import { ResourcePicker } from '@shopify/app-bridge-react';
+import store from 'store-js';
 
 function Index() {
 
     const [model, setModel] = useState({ open: false })
+    const emptyState = !store.get('ids');
+
+    function handleSelection(resources) {
+        const idsFromResources = resources.selection.map((product) => product.id);
+        setModel({ open: false });
+        store.set('ids', idsFromResources);
+        console.log('this is product ids', store.get('ids'))
+    }
 
     return(
         <Page>
@@ -12,7 +21,8 @@ function Index() {
             resourceType="Product"
             showVariants={false}
             open={model.open}
-            onCancel={() => setModel({ open: false })} 
+            onCancel={() => setModel({ open: false })}
+            onSelection={(resources) => handleSelection(resources)} 
             />
             <Layout>
                 <EmptyState
